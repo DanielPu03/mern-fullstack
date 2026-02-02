b41bdbf650c5df5221a479f74853c291e6f9025f
DanielPu03 <trunqphu.209@gmail.com>
2026-02-02 23:31:16 +0700

    Update code

--- backend/controllers/tasksControllers.js
+++ backend/controllers/tasksControllers.js
@@ -1,22 +1,13 @@
 import Task from "../models/Task.js";
 
-
 export const getAllTasks = async (req, res) => {
   try {
     const result = await Task.aggregate([
       {
         $facet: {
-          tasks: [
-            { $sort: { createdAt: -1 } }
-          ],
-          activeCount: [  
-            { $match: { status: 'active' } },
-            { $count: 'count' }
-          ],
-          completeCount: [
-            { $match: { status: 'complete' } },
-            { $count: 'count' }
-          ]
+          tasks: [{ $sort: { createdAt: -1 } }],
+          activeCount: [{ $match: { status: 'active' } }, { $count: 'count' }],
+          completedCount: [{ $match: { status: 'completed' } }, { $count: 'count' }] 
         }
       }
     ]);
@@ -26,14 +17,14 @@ export const getAllTasks = async (req, res) => {
     res.status(200).json({
       tasks: data.tasks || [],
       activeCount: data.activeCount?.[0]?.count || 0,
-      completeCount: data.completeCount?.[0]?.count || 0
+      completedCount: data.completedCount?.[0]?.count || 0
     });
-
   } catch (error) {
     console.error('Lỗi khi gọi getAllTasks:', error);
     res.status(500).json({ message: "Lỗi hệ thống" });
   }
 };
+
 
 
 export const createTask = async (req, res) => {
