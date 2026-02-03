7a99c901a9ecc10ab57a3c89be3e55afee532824
DanielPu03 <trunqphu.209@gmail.com>
2026-02-03 20:42:10 +0700

    continue update

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,35 @@ import { connectDB } from '../config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import path from "path";  
+
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 
 //middlewares
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'}));
+
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({origin: 'http://localhost:5173'}));
+
+}
 
 //routes
 app.use('/api/tasks', tasksRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.send(path.join(__dirname, "../frontend/dist/index.html"));
+  })
+}
 
 //start server
 connectDB().then(() => {
